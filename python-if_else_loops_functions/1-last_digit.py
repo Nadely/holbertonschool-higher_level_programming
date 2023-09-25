6a8942ce09a9c48d3dc6e8aabd366c71c2672336
Nadely <6890@holbertonstudents.com>
2023-09-25 14:06:23 +0200

    change order

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,9 +8,9 @@ if number <= 0:
 else:
     last_digit = int(repr(number)[-1])
 
-if number > 5:
-    print(f"is {number} and is greater than 5")
-elif number < 6 and number != 0:
-    print(f"is {number} and is less than 6 and not 0")
-elif number == 0:
-    print(f"is {number} and is 0")
+elif last_digit == 0:
+    print(f"is {last_digit} and is 0")
+if last_digit < 6 and last_digit != 0:
+    print(f"is {last_digit} is less than 6 and not 0")
+elif last_digit > 5:
+    print(f"is {last_digit} and is greater than 5")
