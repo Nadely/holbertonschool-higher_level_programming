517f3faad0e0ec2c82151816bf7d1e86359fb1e8
Nadely <6890@holbertonstudents.com>
2023-09-25 13:56:19 +0200

    change variable

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,13 +4,13 @@ number = random.randint(-10000, 10000)
 print(f"Last digit of {number} ", end="")
 
 if number <= 0:
-    last_digit = -int(repr(number)[-1])
+    number = -int(repr(number)[-1])
 else:
-    last_digit = int(repr(number)[-1])
+    number = int(repr(number)[-1])
 
-if last_digit < 6 and last_digit != 0:
-    print(f"is {last_digit} is less than 6 and not 0")
-elif last_digit == 0:
-    print(f"is {last_digit} and is 0")
-elif last_digit > 5:
-    print(f"is {last_digit} and is greater than 5")
+if number < 6 and number != 0:
+    print(f"is {number} is less than 6 and not 0")
+elif number == 0:
+    print(f"is {number} and is 0")
+elif number > 5:
+    print(f"is {number} and is greater than 5")
