0ffd42e2641aba1706555e546f1165dc3417c543
Nadely <6890@holbertonstudents.com>
2023-09-26 10:12:57 +0200

    delete space

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for number in range(10):
         if number != number2:
             print("{}{}" .format(number, number2), end=', '
                   if number != 8 or number2 != 9 else "")
-print(" ")
+print("")
