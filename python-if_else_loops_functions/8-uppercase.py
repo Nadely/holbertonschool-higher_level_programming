d7a7d00eec222753a43bd240ba8e4cc7e668c993
Nadely <6890@holbertonstudents.com>
2023-09-26 12:10:21 +0200

    reversal of lines 2 and 3

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-letter_uppercase = 0
 def uppercase(str):
+    letter_uppercase = 0
     for letter in str:
         if ord('a') <= ord(letter) <= ord('z'):
             letter_uppercase = ord(letter) - 32
