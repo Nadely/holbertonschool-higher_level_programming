06827a2893ea8be8352145da442e98136312f589
Nadely <6890@holbertonstudents.com>
2023-09-29 09:53:21 +0200

    program that imports all functions from the file calculator_1.py and handles basic operations.

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    arg = len(sys.argv)
+    arg = len(argv)
     if arg != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <argv[2]> <b>")
         exit(1)
-    operator = sys.argv[2]
-    if operator != "+" and operator != "-" and operator != "*" and
-    operator != "/":
-        print("Unknown operator. Available operators: +, -, * and /")
+    if argv[2] != "+" and argv[2] != "-" and argv[2] != '*' and argv[2] != "/":
+        print("Unknown argv[2]. Available argv[2]s: +, -, * and /")
         exit(1)
-    if operator == "+":
+    if argv[2] == "+":
         print("{}".format(add(int(sys.argv[1]), int(sys.argv[3]))))
-    elif operator == "-":
+    elif argv[2] == "-":
         print("{}".format(sub(int(sys.argv[1]), int(sys.argv[3]))))
-    elif operator == "*":
+    elif argv[2] == '*':
         print("{}".format(mul(int(sys.argv[1]), int(sys.argv[3]))))
-    elif operator == "/":
+    elif argv[2] == "/":
         print("{}".format(div(int(sys.argv[1]), int(sys.argv[3]))))
