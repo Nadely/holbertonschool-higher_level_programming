0bdc965e797f1e2db68ae725460cf42230daa87a
Nadely <6890@holbertonstudents.com>
2023-09-29 09:53:58 +0200

    program that imports all functions from the file calculator_1.py and handles basic operations.

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -4,10 +4,10 @@ from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     arg = len(argv)
     if arg != 4:
-        print("Usage: ./100-my_calculator.py <a> <argv[2]> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     if argv[2] != "+" and argv[2] != "-" and argv[2] != '*' and argv[2] != "/":
-        print("Unknown argv[2]. Available argv[2]s: +, -, * and /")
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     if argv[2] == "+":
         print("{}".format(add(int(sys.argv[1]), int(sys.argv[3]))))
