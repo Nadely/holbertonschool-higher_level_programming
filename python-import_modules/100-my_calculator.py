5b4c7d272722edc3f93d98947075b27cae1f53fd
Nadely <6890@holbertonstudents.com>
2023-09-29 10:02:17 +0200

    program that imports all functions from the file calculator_1.py and handles basic operations.

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -10,10 +10,14 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     if argv[2] == "+":
-        print("{}".format(add(int(argv[1]), int(argv[3]))))
+        print("{} + {} = {}".format(int(argv[1]), int(argv[3]),
+                                        add(int(argv[1]), int(argv[3]))))
     elif argv[2] == "-":
-        print("{}".format(sub(int(argv[1]), int(argv[3]))))
+        print("{} - {} = {}".format(int(argv[1]), int(argv[3]),
+                                        sub(int(argv[1]), int(argv[3]))))
     elif argv[2] == '*':
-        print("{}".format(mul(int(argv[1]), int(argv[3]))))
+        print("{} * {} = {}".format(int(argv[1]), int(argv[3]),
+                                        mul(int(argv[1]), int(argv[3]))))
     elif argv[2] == "/":
-        print("{}".format(div(int(argv[1]), int(argv[3]))))
+        print("{} / {} = {}".format(int(argv[1]), int(argv[3]),
+                                        div(int(argv[1]), int(argv[3]))))
