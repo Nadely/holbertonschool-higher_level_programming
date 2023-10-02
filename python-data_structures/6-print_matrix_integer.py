875466ec916c175682567758dc575bc653d82984
Nadely <6890@holbertonstudents.com>
2023-10-02 14:45:23 +0200

    correction function that prints a matrix of integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    len_row = len(matrix)
     for row in matrix:
-        for idx in row:
-            print("{:d}".format(idx), end=' ')
+        for idx, num in enumerate(row):
+            print("{:d}".format(num), end='')
+            if idx < len(row) - 1:
+                print(' ', end='')
+        if len_row > 1:
+            print()
+    if not matrix:
         print()
