0db2bef6c6f10ac356250b0dd096f774563496a6
Nadely <6890@holbertonstudents.com>
2023-10-02 17:49:57 +0200

    add one condition if my list is none

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for interger in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[interger]))
+    if not my_list:
+        return
+    for integer in reversed(my_list):
+        print("{:d}".format(integer))
