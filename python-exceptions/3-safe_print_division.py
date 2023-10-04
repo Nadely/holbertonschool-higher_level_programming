216b8fba160b4284997f006eb60499ce87ee41fd
Nadely <6890@holbertonstudents.com>
2023-10-04 22:10:50 +0200

    delete space line 2 and at end function

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def safe_print_division(a, b) :
+def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
@@ -9,4 +9,3 @@ def safe_print_division(a, b) :
         print("Inside result : {}".format(result))
     finally:
         return result
-
