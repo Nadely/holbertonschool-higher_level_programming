71d57629f34cafb4f4059865dcce650fac758ebe
Nadely <6890@holbertonstudents.com>
2023-10-04 22:34:33 +0200

    modification function for change place result when result is 0 or not

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python3
 def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Inside result : None")
         result = None
-    else:
-        print("Inside result : {}".format(result))
     finally:
-        print("Inside result : {}".format(result))
+        if result is not None:
+            print("Inside result: {}".format(result))
+        else:
+            print("Inside result: {}".format(None))
         return result
