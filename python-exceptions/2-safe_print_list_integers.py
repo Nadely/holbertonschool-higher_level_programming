683c1b309a78d4b09bf28463a75fa0426cc5fffe
Nadely <6890@holbertonstudents.com>
2023-10-05 15:24:12 +0200

    function that raises a name exception with a message

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
     index = 0
-    while count < x:
+    count = 0
+    while index < x:
         try:
             print("{:d}".format(my_list[index]), end='')
             count += 1
