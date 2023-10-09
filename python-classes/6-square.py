6de779b30f065e22492920211e86ed5d58382863
Nadely <6890@holbertonstudents.com>
2023-10-09 16:06:25 +0200

    update Coordinates of a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -5,15 +5,12 @@ this class is empty
 
 
 class Square():
-    """line defining that it is a class is empty"""
-    {}
-
     """Private instance attribute: size"""
 
-    def __init__(self, _Square__size=0, _position=(0, 0)):
+    def __init__(self, _Square__size=0, position=(0, 0)):
         """Instantiation with size and position"""
         self._Square__size = _Square__size
-        self.__position = _position
+        self.position = position
 
     """Public instance method"""
 
@@ -47,10 +44,10 @@ class Square():
         if self._Square__size == 0:
             print()
         else:
-            for _ in range(self.__position[1]):
+            for _ in range(self.position[1]):
                 print()
             for _ in range(self._Square__size):
-                print(" " * self.__position[0] + "#" * self._Square__size)
+                print(" " * self.position[0] + "#" * self._Square__size)
 
         """property getter"""
 
@@ -66,6 +63,7 @@ class Square():
         """Instantiation with optional position"""
         if isinstance(value, tuple) and len(value) == 2:
             if isinstance(value[0], int) and isinstance(value[1], int):
-                self.__position = value
-        else:
-            raise TypeError("position must be a tuple of 2 positive integers")
+                if value[0] >= 0 and value[1] >= 0:
+                    self.__position = value
+                    return
+        raise TypeError("position must be a tuple of 2 positive integers")
