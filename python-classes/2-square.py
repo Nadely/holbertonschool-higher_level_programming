3538f61746f149d5f210c88386d08f5635fc830d
Nadely <6890@holbertonstudents.com>
2023-10-09 12:17:43 +0200

    Write a class Square that defines a square with option

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""class Square that defines a square
+this class is empty
+"""
+
+
+class Square():
+    """line defining that it is a class is empty"""
+    {}
+
+    """Private instance attribute: size"""
+
+    def __init__(self, _Square__size=0):
+        """Instantiation with optional size"""
+        if not isinstance(_Square__size, int):
+            raise TypeError("size must be an integer")
+        if _Square__size < 0:
+            raise ValueError("must be >= 0")
+        """Instantiation with size"""
+        self._Square__size = _Square__size
