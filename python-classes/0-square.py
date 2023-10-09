1f2cc84f2fafde4c3f09e1ac4683d6241e6a9557
Nadely <6890@holbertonstudents.com>
2023-10-09 10:57:48 +0200

    commentary

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-# class Square that defines a square
-class Square: # line defining that it is a class and its name
+"""class Square that defines a square
+this class is empty
+"""
+class Square():
+    """line defining that it is a class is empty"""
     {}
