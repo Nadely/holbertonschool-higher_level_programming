1f18c06c31ead0228cae6009ab510a502df01303
Nadely <6890@holbertonstudents.com>
2023-10-09 15:58:23 +0200

    update Coordinates of a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -65,8 +65,7 @@ class Square():
     def position(self, value):
         """Instantiation with optional position"""
         if len(value) == 2 and isinstance(value, tuple):
-            self.__position = value
-        if isinstance(value[0], int) and isinstance(value[1], int):
-            self.__position = value
+            if isinstance(value[0], int) and isinstance(value[1], int):
+                self.__position = value
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
