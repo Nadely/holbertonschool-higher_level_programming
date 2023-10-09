14e78377584ebdc0ac298e79cc641903806fabde
Nadely <6890@holbertonstudents.com>
2023-10-09 12:37:17 +0200

    returns the current square area

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""class Square that defines a square
+this class is empty
+"""
+
+
+class Square():
+    """line defining that it is a class is empty"""
+    {}
+
+    """Private instance attribute: size"""
+
+    def __init__(self, _Square__size=0):
+        """Instantiation with optional size"""
+        if not isinstance(_Square__size, int):
+            raise TypeError("size must be an integer")
+        if _Square__size < 0:
+            raise ValueError("size must be >= 0")
+        """Instantiation with size"""
+        self._Square__size = _Square__size
+
+    """Public instance method"""
+
+    def area(self):
+        """Compute the area of the square"""
+        return self._Square__size ** 2
