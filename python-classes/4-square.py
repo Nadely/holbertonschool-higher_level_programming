e9357dd06da604dfdffb5b445e27f733bbd52c93
Nadely <6890@holbertonstudents.com>
2023-10-09 13:26:50 +0200

    property setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,40 @@
 #!/usr/bin/python3
+"""class Square that defines a square
+this class is empty
+"""
+
+
+class Square():
+    """line defining that it is a class is empty"""
+    {}
+
+    """Private instance attribute: size"""
+
+    def __init__(self, _Square__size=0):
+        """Instantiation with size"""
+        self._Square__size = _Square__size
+
+    """Public instance method"""
+
+    def area(self):
+        """Compute the area of the square"""
+        return self._Square__size ** 2
+
+    """property getter"""
+
+    @property
+    def size(self):
+        """return getter"""
+        return self._Square__size
+
+    """property setter"""
+
+    @size.setter
+    def size(self, value):
+        """Instantiation with optional size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        """Change name"""
+        self._Square__size = value
