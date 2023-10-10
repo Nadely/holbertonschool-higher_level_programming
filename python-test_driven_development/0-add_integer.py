44953c513bc54db95d05f6d54ac84d0f4702a955
Nadely <6890@holbertonstudents.com>
2023-10-10 11:53:53 +0200

    Integers addition and test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
+"""
+Function for adds two integers, a and b must be integers or floats,
+otherwise raise a TypeError exception with the message :
+'a must be an integer or b must be an integer'
+a and b must be first casted to integers if they are float """
+
+
 def add_integer(a, b=98):
+    """ if a or b is not int or float return message error
+    then check if a or b is a flaot convert in int
+    finish adds a and b """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    result = a + b
+    return result
