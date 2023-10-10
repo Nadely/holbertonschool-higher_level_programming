313df6758ebc338d0a58bb86ad404613dbbd1d15
Nadely <6890@holbertonstudents.com>
2023-10-10 11:57:59 +0200

    correction Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,11 +12,8 @@ def add_integer(a, b=98):
     finish adds a and b """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    result = a + b
-    return result
+    a = int(a)
+    b = int(b)
+    return a + b
