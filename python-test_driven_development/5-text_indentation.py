2f3ec4b7702171c375e249faacf7a5231ed05a87
Nadely <6890@holbertonstudents.com>
2023-10-10 22:57:22 +0200

    Text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,2 +1,32 @@
 #!/usr/bin/python3
+""" function that prints a text with 2 new lines after each of
+these characters: ".", "?" and ":"
+>>> text_indentation("Hello, who's that ? It's me !")
+Hello, who's that ?
+
+It's me !
+"""
+
+
 def text_indentation(text):
+    """ text must be a string, otherwise raise a TypeError exception
+    with the message: "text must be a string"
+    There should be no space at the beginning or at the end of
+    each printed line """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    symboles = {".", "?", ":"}
+    indentation = "\n"
+    result = ""
+    new_line = True
+    for s in text:
+        if s in symboles:
+            result += s + "\n" + indentation
+            new_line = True
+        elif s == " " and new_line:
+            pass
+        else:
+            result += s
+            new_line = False
+    print(result)
