7e428ca3e1fb02b541f7528c8b9c65c6a14b6e57
Nadely <6890@holbertonstudents.com>
2023-10-11 11:38:04 +0200

    add test for Integers addition

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import unittest
+
 """Module to find the max integer in a list
 """
 
@@ -7,6 +9,7 @@ def max_integer(list=[]):
     """Function to find and return the max integer in a list of integers
         If the list is empty, the function returns None
     """
+
     if len(list) == 0:
         return None
     result = list[0]
@@ -16,3 +19,19 @@ def max_integer(list=[]):
             result = list[i]
         i += 1
     return result
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max_integer(self):
+        result = max_integer([1, 2, 3, 4])
+        self.assertEqual(result, 4)
+
+    def test_list_is_empty(self):
+        result = max_integer([])
+        self.assertEqual(result, None)
+
+    def test_integer_is_identical(self):
+        result = max_integer([4, 4, 4, 4])
+        self.assertEqual(result, 4)
+
+if __name__ == '__main__':
+    unittest.main()
