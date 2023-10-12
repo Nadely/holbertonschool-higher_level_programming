4e39b57a9df4828ee68de624659274e5706c65db
Nadely <6890@holbertonstudents.com>
2023-10-12 11:47:43 +0200

    A square is a rectangle

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -113,4 +113,4 @@ class Rectangle:
     """class method : square is a rectangle"""
     @classmethod
     def square(cls, size=0):
-        return cls(width=size, height=size)
+        return cls(size, size)
