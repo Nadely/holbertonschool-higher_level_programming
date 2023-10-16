292a86856044d18b17d4a8ef1e4fec4ead3d55ac
Nadely <6890@holbertonstudents.com>
2023-10-16 20:59:23 +0200

    My list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""class MyList that inherits from list"""
+
+
+class MyList(list):
+    """Public instance method that prints the list,
+    but sorted (ascending sort)"""
+
+    def print_sorted(self) :
+        print(sorted(self))
