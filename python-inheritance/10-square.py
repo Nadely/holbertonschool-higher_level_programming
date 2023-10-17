ce1f04578e839748d7fd5d7a7fddee93ae444e0d
Nadely <6890@holbertonstudents.com>
2023-10-17 16:57:48 +0200

    Square #1

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -43,8 +43,8 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """size must be a positive integer, validated by integer_validator"""
-
-        self.integer_validator("size", size)
+        if isinstance(size, int):
+            self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
