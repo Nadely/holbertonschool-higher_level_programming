5d4e1377e035e263abb6c020b0703da5f521f1f5
Nadely <6890@holbertonstudents.com>
2023-10-17 14:21:57 +0200

    Integer validator and Rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""A class BaseGeometry"""
+
+
+class BaseGeometry:
+    """def in class"""
+
+    def area(self):
+        """that raises an Exception"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value, assuming name is always a string"""
+
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """def in class"""
+
+    def __init__(self, width, height):
+        """width and height must be positive integers,
+        validated by integer_validator0"""
+
+        self.__width = width
+        self.__height = height
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
