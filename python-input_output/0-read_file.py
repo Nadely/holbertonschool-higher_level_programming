03628ceafea6be566e80d19d9cb639dc3449da50
Nadely <6890@holbertonstudents.com>
2023-10-17 19:24:02 +0200

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""function that reads a text file"""
+
+
 def read_file(filename=""):
+    """use the with statement"""
+
     with open(filename, "r") as file:
         print(file.read())
