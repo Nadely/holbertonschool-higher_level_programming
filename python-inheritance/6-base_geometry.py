8417acc8a2838ec9853928286f059ea015042658
Nadely <6890@holbertonstudents.com>
2023-10-17 12:00:32 +0200

    Improve Geometry

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""that raises an Exception with the message area() is not implemented"""
+
+
+class BaseGeometry:
+    """that raises an Exception"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
