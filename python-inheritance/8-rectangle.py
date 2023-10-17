cb98a21a9096f830f7b925d0893956e5a3c2926e
Nadely <6890@holbertonstudents.com>
2023-10-17 14:41:02 +0200

    Rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -26,7 +26,7 @@ class Rectangle(BaseGeometry):
         """width and height must be positive integers,
         validated by integer_validator0"""
 
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
