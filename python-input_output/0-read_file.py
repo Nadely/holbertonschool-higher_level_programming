4bbe0be6d4fe25a150dad3b2882b701470324b93
Nadely <6890@holbertonstudents.com>
2023-10-17 19:27:52 +0200

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """use the with statement"""
 
-    with open(filename, "r") as file:
+    with open(filename, encoding='utf-8') as file:
         print(file.read())
