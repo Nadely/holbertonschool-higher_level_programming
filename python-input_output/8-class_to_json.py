99d30d96accd69ba3a08a65ac41717f17c397798
Nadely <6890@holbertonstudents.com>
2023-10-18 21:53:45 +0200

    Class to JSON add description

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""Write a function that returns the dictionary description with simple
+ata structure (list, dictionary, string, integer and boolean)
+for JSON serialization of an object"""
+
+
 import json
 
+
 def class_to_json(obj):
+    """obj is an instance of a Class"""
+
     dictionnary = {}
     if hasattr(obj, "__dict__"):
         dictionnary = obj.__dict__.copy()
