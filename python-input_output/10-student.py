eebe4e1ef3de17913f586fd1d093c5a19bae8f78
Nadely <6890@holbertonstudents.com>
2023-10-18 22:34:57 +0200

    Student to JSON with filter

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""a class Student that defines a studen"""
+
+
+class Student:
+    """Public instance attributes"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (isinstance(attrs, list)) and (all((type(x) is str) for x in attrs)):
+            return {name: getattr(self, name) for name in attrs
+                    if hasattr(self, name)}
+        """retourne chaque attributs qui sont dans attrs si
+            l'attribut existe dans la class.
+            'name' est la variable qui recupère l'attribut dans la boucle"""
+
+        return self.__dict__
