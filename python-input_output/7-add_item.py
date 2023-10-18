e350bbdbde9fc96081dd8113a8d35767288110fd
Nadely <6890@holbertonstudents.com>
2023-10-18 21:03:55 +0200

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,20 +11,18 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def adds_all_args(argv):
-    """The list must be saved as a JSON representation in a file
+"""The list must be saved as a JSON representation in a file
     named add_item.json"""
 
+if __name__ == "__main__":
     try:
         args_list = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        args_list = [] + '\n'
+        args_list = []
 
-    args_list.extend(argv)
-    save_to_json_file(args_list, "add_item.json")
-
-if __name__ == "__main__":
     argv = sys.argv[1:]
 
     if argv:
-        adds_all_args(argv)
+        args_list.extend(argv)
+
+    save_to_json_file(args_list, "add_item.json")
