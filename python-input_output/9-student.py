59f687eb97446818c1797fa84b458083c2757a6d
Nadely <6890@holbertonstudents.com>
2023-10-18 22:04:56 +0200

    Student to JSON

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""a class Student that defines a studen"""
+
+
+class Student:
+    """Public instance attributes"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        dictionnary = {}
+        if hasattr(self, "__dict__"):
+            dictionnary = self.__dict__.copy()
+        return dictionnary
