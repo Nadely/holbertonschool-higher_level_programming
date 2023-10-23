0a8b44e46e99ab288fba6d45a79d5d38a37238ae
Nadely <6890@holbertonstudents.com>
2023-10-23 12:48:43 +0200

    __str__

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -91,3 +91,8 @@ class Rectangle(Base):
             for _ in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """Print str"""
+        return "[Rectangle] ({}) {}/{} -{}/{}".format(
+                self.id, self.x,self.y, self.__width, self.__height)
