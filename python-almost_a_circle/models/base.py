831bfb7e465ca96d27a056feb7a2bb649ddb402a
Nadely <6890@holbertonstudents.com>
2023-10-24 10:16:05 +0200

    Dictionary to JSON string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,10 +19,10 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None:
-            return "[]"
-        else:
+        try :
             return json.dumps(list_dictionaries)
+        except FileNotFoundError:
+            return "[]"
 
     @classmethod
     def save_to_file(cls, list_objs):
