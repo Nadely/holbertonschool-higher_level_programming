71699ecaa0fa07c05fa45689bf7581f6e8314644
Nadely <6890@holbertonstudents.com>
2023-10-25 13:14:30 +0200

    tests 4

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -42,7 +42,6 @@ class TestBase(unittest.TestCase):
         json_str = Base.to_json_string(None)
         self.assertEqual(json_str, "[]")
 
-
     def test_save_to_file_with_empty_data(self):
         data = []
         filename = "Base.json"
@@ -65,6 +64,52 @@ class TestBase(unittest.TestCase):
             self.assertEqual(saved_data, [])
 
         os.remove(filename)
+
+    def test_from_json_string_with_data(self):
+        data = [{'key1': 'value1', 'key2': 'value2'}]
+        json_str = json.dumps(data)
+        loaded_data = Base.from_json_string(json_str)
+        self.assertEqual(loaded_data, data)
+
+    def test_from_json_string_with_empty_data(self):
+        json_str = "[]"
+        loaded_data = Base.from_json_string(json_str)
+        self.assertEqual(loaded_data, [])
+
+    def test_from_json_string_with_none_data(self):
+        loaded_data = Base.from_json_string(None)
+        self.assertEqual(loaded_data, [])
+
+    def test_create_with_attributes(self):
+        data = {'attr1': 10, 'attr2': 'example'}
+        instance = Base.create(**data)
+        self.assertEqual(instance.attr1, 10)
+        self.assertEqual(instance.attr2, 'example')
+
+    def test_create_without_attributes(self):
+        instance = Base.create()
+        self.assertEqual(instance.attr1, 1)
+        self.assertEqual(instance.attr2, 1)
+
+    def test_load_from_file_with_data(self):
+        data = [{'attr1': 10, 'attr2': 'example'}, {'attr1': 20, 'attr2': 'test'}]
+        filename = "Base.json"
+
+        # Create a JSON file with the test data
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(json.dumps(data))
+
+        instances = Base.load_from_file()
+
+        self.assertEqual(len(instances), 2)
+        self.assertEqual(instances[0].attr1, 10)
+        self.assertEqual(instances[0].attr2, 'example')
+        self.assertEqual(instances[1].attr1, 20)
+        self.assertEqual(instances[1].attr2, 'test')
+
+    def test_load_from_file_with_no_file(self):
+        instances = Base.load_from_file()
+        self.assertEqual(instances, [])
 
 if __name__ == "__main__":
     unittest.main()
