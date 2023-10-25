84593a55601166213df2ce4a7517bd7fe87a1a75
Nadely <6890@holbertonstudents.com>
2023-10-25 13:30:00 +0200

    tests 6

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -19,5 +19,52 @@ class TestSquare(unittest.TestCase):
             pass
         del self.s
 
+    def test_init_with_valid_values(self):
+        square = Square(5, 2, 3, 1)
+        self.assertEqual(square.size, 5)
+        self.assertEqual(square.x, 2)
+        self.assertEqual(square.y, 3)
+        self.assertEqual(square.id, 1)
+
+    def test_init_with_invalid_size(self):
+        with self.assertRaises(ValueError):
+            square = Square(0)
+
+    def test_set_size_with_valid_value(self):
+        square = Square(5)
+        square.size = 10
+        self.assertEqual(square.size, 10)
+
+    def test_set_size_with_invalid_value(self):
+        square = Square(5)
+        with self.assertRaises(ValueError):
+            square.size = -5
+
+    def test_str(self):
+        square = Square(5, 2, 3, 1)
+        self.assertEqual(str(square), "[Square] (1) 2/3 - 5")
+
+    def test_update_with_args(self):
+        square = Square(5)
+        square.update(1, 10, 2, 3)
+        self.assertEqual(square.id, 1)
+        self.assertEqual(square.size, 10)
+        self.assertEqual(square.x, 2)
+        self.assertEqual(square.y, 3)
+
+    def test_update_with_kwargs(self):
+        square = Square(5)
+        square.update(id=1, size=10, x=2, y=3)
+        self.assertEqual(square.id, 1)
+        self.assertEqual(square.size, 10)
+        self.assertEqual(square.x, 2)
+        self.assertEqual(square.y, 3)
+
+    def test_to_dictionary(self):
+        square = Square(5, 2, 3, 1)
+        square_dict = square.to_dictionary()
+        expected_dict = {'id': 1, 'size': 5, 'x': 2, 'y': 3}
+        self.assertEqual(square_dict, expected_dict)
+
 if __name__ == "__main__":
     unittest.main()
