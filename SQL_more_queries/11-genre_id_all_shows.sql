e80069c87f322feecf9405250c1cbae8dba6ac37
Nadely <6890@holbertonstudents.com>
2023-11-21 14:38:34 +0100

    Genre ID for all shows

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,5 +2,6 @@
 USE hbtn_0d_tvshows;
 SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
 FROM hbtn_0d_tvshows.tv_shows
-LEFT JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN hbtn_0d_tvshows.tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, IFNULL(tv_show_genres.genre_id, 'NULL') ASC;
