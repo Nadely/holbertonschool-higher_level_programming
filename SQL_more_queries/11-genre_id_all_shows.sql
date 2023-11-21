50a288bab7e2ce55510a7d9514c23408f2b997fa
Nadely <6890@holbertonstudents.com>
2023-11-21 14:27:12 +0100

    Genre ID for all shows

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
--- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
+-- Use hbtn_0d_tvshows.
 USE hbtn_0d_tvshows;
+-- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
 SELECT tv_shows.title, COALESCE(tv_show_genres.genre_id, 'NULL') as genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
