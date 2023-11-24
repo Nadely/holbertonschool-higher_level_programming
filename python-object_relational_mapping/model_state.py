50afc13ec26fcf8f620567e2300cada72b8d3430
Nadely <6890@holbertonstudents.com>
2023-11-24 17:46:21 +0100

    Correction of tasks 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,6 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 # Create a base class for declarative models
 Base = declarative_base()
 
+
 # Define the State class, representing the 'states' table in the database
 class State(Base):
     """Represents a state in the database. Attributes:
