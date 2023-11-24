d8e7772f816785353490d4dbfbd75eff7c6286db
Nadely <6890@holbertonstudents.com>
2023-11-24 22:16:52 +0100

    Cities in state

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""Script to fetch and display City objects from the database"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+    # Create a connection to the database
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+
+    # Create the tables in the database
+    Base.metadata.create_all(engine)
+
+    # Create a session to interact with the database
+    session = Session(engine)
+
+    # Query the database and print City objects
+    cities = session.query(City).order_by(City.id).all()
+
+    #Print result
+    for city in cities:
+        state_name = session.query(State.name).filter(State.id ==
+                                                      city.state_id).scalar()
+        print("{}: ({}) {}".format(state_name, city.id, city.name))
+
+    # Close the session
+    session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""Start link class to table in database """
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+# Create a base class for declarative models
+Base = declarative_base()
+
+
+# Define the City class, representing the 'cities' table in the database
+class City(Base):
+    """Represents a cities in the database. Attributes:
+    - id: The unique identifier for the cities.
+    - name: The name of the cities.
+    - state_id: The foreign key to the states table."""
+
+    __tablename__ = 'cities'
+    id = Column("id", Integer, primary_key=True, nullable=False,
+                autoincrement=True)
+    name = Column("name", String(128), nullable=False)
+    state_id = Column("state_id", Integer, ForeignKey('states.id'),
+                      nullable=False)
+
+    def __repr__(self):
+        return f"<City(id={self.id}, name={self.name}, state_id={
+            self.state_id})>"
