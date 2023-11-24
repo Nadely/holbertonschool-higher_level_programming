51dccbe75344644911d4883e9f38558e9ef2ef95
Nadely <6890@holbertonstudents.com>
2023-11-24 16:26:38 +0100

    Add of task 0 at 6

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Write a script that takes in the name of a state as an argument and lists
+all cities of that state, using the database hbtn_0e_4_usa"""
+
+
+import MySQLdb
+import sys
+
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                       passwd=sys.argv[2], db=sys.argv[3])
+cur = conn.cursor()
+cur.execute("SELECT cities.name FROM cities "
+            "JOIN states ON cities.state_id = states.id "
+            "WHERE states.name = %(arg)s ORDER BY cities.id",
+            {'arg': sys.argv[4]})
+
+
+query_rows = cur.fetchall()
+cities = [row[0] for row in query_rows]
+print(", ".join(cities))
+
+cur.close()
+conn.close()
