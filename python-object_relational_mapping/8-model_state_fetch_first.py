c6ea5600c8e41041adc414d28e67f17e3314d957
Nadely <6890@holbertonstudents.com>
2023-11-24 18:13:38 +0100

    First state

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""Start link class to table in database"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    # Create an engine to connect to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Create all tables in the database
+    Base.metadata.create_all(engine)
+
+    # Create a session to execute SQL commands
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query the database and print results
+    for first_state in session.query(State).where(State.id).first():
+        if first_state:
+            print("{}: {}".format(first_state.id, first_state.name))
+        else:
+            print("Nothing")
+
+    # Close the session
+    session.close()
+
