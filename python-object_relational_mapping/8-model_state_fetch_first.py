e8a8c610673d794302d1f80646757e4f5b523d10
Nadely <6890@holbertonstudents.com>
2023-11-24 18:15:04 +0100

    Correction of tasks 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database"""
+"""Prints the first State object from the database hbtn_0e_6_usa"""
 
 import sys
 from model_state import Base, State
@@ -12,20 +12,16 @@ if __name__ == "__main__":
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
-    # Create all tables in the database
-    Base.metadata.create_all(engine)
-
     # Create a session to execute SQL commands
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # Query the database and print results
-    for first_state in session.query(State).where(State.id).first():
-        if first_state:
-            print("{}: {}".format(first_state.id, first_state.name))
-        else:
-            print("Nothing")
+    first_state = session.query(State).order_by(State.id).first()
+    if first_state:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
 
     # Close the session
     session.close()
-
