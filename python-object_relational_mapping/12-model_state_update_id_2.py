6985d418d08d01b23ca59217ea8ac1d28227a82d
Nadely <6890@holbertonstudents.com>
2023-11-24 21:53:00 +0100

    Update a state

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+"""script that prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    # Create an engine to connect to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Create a session to execute SQL commands
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+     # Query the State object with id = 2
+    state_to_update = session.query(State).filter_by(id=2).first()
+
+    # Check if the State object with id = 2 exists
+    if state_to_update:
+        # Update the name of the State to "New Mexico"
+        state_to_update.name = "New Mexico"
+
+    # Commit the session to persist the changes to the database
+        session.commit()
+
+    # Print result
+        print("{}".format(state_to_update.id))
+    else:
+        print("Not found")
+
+    # Close the session
+    session.close()
